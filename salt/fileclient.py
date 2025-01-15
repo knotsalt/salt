14dede152e4a92e55c354112b6940f1aec91a637
Erik Johnson <erjohnso@akamai.com>
2025-01-15 10:10:17 -0600

    Apply fileclient_timeout_logging patch

--- salt/fileclient.py
+++ salt/fileclient.py
@@ -1160,7 +1160,7 @@ class RemoteClient(Client):
             )
         except salt.exceptions.SaltReqTimeoutError:
             raise SaltClientError(
-                f"File client timed out after {int(time.time() - start)}"
+                f"File client timed out after {int(time.monotonic() - start)} seconds"
             )
 
     def destroy(self):
